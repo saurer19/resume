360b0b8b9926171b8d21b7b5b1b60d6ddf65be86
Gabriel Rivas <saurer19@gmail.com>
2019-10-06 12:10:26 -0400

    navigation

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="antialiased text-gray-900">
+    <div class="bg-gray-200 min-h-screen">
+      <Navbar />
+      <div>
+      <router-view />
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  name: "app",
+  components: {
+    Navbar
   }
-}
-</style>
+};
+</script>
+<style src="./assets/tailwind.css"></style>
+
---
+++ src/views/Skills.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="skills">
+    <h1>This is an skils page</h1>
+  </div>
+</template>
