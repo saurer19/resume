00ccc85488a20bcaad62226d9e82c6dcba7bc7dc
Gabriel Rivas <saurer19@gmail.com>
2019-10-12 17:38:33 -0400

    better card for skills

--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -2,27 +2,55 @@
   <div class="flex-1 h-full pt-8 px-6 justify-center bg-gray-200 text-gray-800">
     <div class="text-3xl font-light">Skills</div>
     <div class="flex flex-col pt-6 justify-center content-center">
-      <div class="max-w-md md:flex rounded overflow-hidden shadow-lg mb-4 mx-auto">
-        <div class="w-full bg-blue-300 h-24">Carousel Icons</div>
-        <div class="font-bold text-xl mb-2">JavaScript</div>
-        <div class="text-gray-700 text-base">Description of JS experience</div>
-      </div>
-      <div class="max-w-md rounded overflow-hidden shadow-lg mb-4 mx-auto">
-        <div class="w-full bg-blue-300 h-24">Carousel Icons</div>
-        <div class="font-bold text-xl mb-2">HTML/CSS</div>
+      <div class="max-w-lg w-full md:flex mx-auto my-4">
         <div
-          class="text-gray-700 text-base"
-        >Description of front-end experience, include Tailwind and atomic frameworks</div>
+          class="h-64 md:h-auto md:w-48 flex-none rounded-t md:rounded-t-none md:rounded-l text-center overflow-hidden bg-blue-900"
+        >
+          >
+          <div class="flex py-3">
+            <img class="h-16 w-full" src="@/assets/react-logo.svg" alt="react.js logo" />
+            <img class="h-16 w-full" src="@/assets/react-logo.svg" alt="react.js logo" />
+          </div>
+        </div>
+        <div
+          class="w-full border-r border-b border-l border-gray-300 md:border-l-0 md:border-t bg-white rounded-b md:rounded-b-none md:rounded-r p-4 flext flex-col justify-between leading-normal"
+        >
+          <div class="font-bold text-xl mb-2 p-2">JavaScript</div>
+          <div class="text-gray-700 text-base p-2">Description of JS experience</div>
+        </div>
       </div>
-      <div class="max-w-md rounded overflow-hidden shadow-lg mb-4 mx-auto">
-        <div class="w-full bg-blue-300 h-24">Carousel Icons</div>
-        <div class="font-bold text-xl mb-2">CI/CD Integration</div>
-        <div class="text-gray-700 text-base">Description of aws experience, pipeline deployment</div>
+      <div class="max-w-md w-full md:flex mx-auto my-4">
+        <div
+          class="h-48 md:h-auto md:w-48 flex-none rounded-t md:rounded-t-none md:rounded-l text-center overflow-hidden bg-blue-400"
+        >Carousel Icons</div>
+        <div
+          class="border-r border-b border-l border-gray-300 md:border-l-0 md:border-t bg-white rounded-b md:rounded-b-none md:rounded-r p-4 flext flex-col justify-between leading-normal"
+        >
+          <div class="font-bold text-xl mb-2 p-2">JavaScript</div>
+          <div class="text-gray-700 text-base p-2">Description of JS experience</div>
+        </div>
       </div>
-      <div class="max-w-md rounded overflow-hidden shadow-lg mb-4 mx-auto">
-        <div class="w-full bg-blue-300 h-24">Carousel Icons</div>
-        <div class="font-bold text-xl mb-2">FullStack</div>
-        <div class="text-gray-700 text-base">Description of aws experience, pipeline deployment</div>
+      <div class="max-w-md w-full md:flex mx-auto my-4">
+        <div
+          class="h-48 md:h-auto md:w-48 flex-none rounded-t md:rounded-t-none md:rounded-l text-center overflow-hidden bg-blue-400"
+        >Carousel Icons</div>
+        <div
+          class="border-r border-b border-l border-gray-300 md:border-l-0 md:border-t bg-white rounded-b md:rounded-b-none md:rounded-r p-4 flext flex-col justify-between leading-normal"
+        >
+          <div class="font-bold text-xl mb-2 p-2">JavaScript</div>
+          <div class="text-gray-700 text-base p-2">Description of JS experience</div>
+        </div>
+      </div>
+      <div class="max-w-md w-full md:flex mx-auto my-4">
+        <div
+          class="h-48 md:h-auto md:w-48 flex-none rounded-t md:rounded-t-none md:rounded-l text-center overflow-hidden bg-blue-400"
+        >Carousel Icons</div>
+        <div
+          class="border-r border-b border-l border-gray-300 md:border-l-0 md:border-t bg-white rounded-b md:rounded-b-none md:rounded-r p-4 flext flex-col justify-between leading-normal"
+        >
+          <div class="font-bold text-xl mb-2 p-2">JavaScript</div>
+          <div class="text-gray-700 text-base p-2">Description of JS experience</div>
+        </div>
       </div>
     </div>
   </div>
