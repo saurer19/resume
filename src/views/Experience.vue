a2a17cdfc8364a62d25da4a63dc2f457a1f7a4fb
Gabriel Rivas <gabrielrivas@Gabriels-Mac-mini.local>
2019-10-14 19:32:08 -0400

    experience card

--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -2,6 +2,35 @@
   <div class="flex-1 h-full pt-8 px-6 justify-center bg-gray-200 text-gray-800">
     <div class="experience">
       <h1>This is an experience page</h1>
+      <div
+        class="lg:-mx-12 mt-6 sm:mt-12 bg-white rounded overflow-hidden shadow"
+      >
+        <div class="md:flex">
+          <div class="md:w-1/2 md:pt-16 md:pr-0 md:pl-12">
+            <a
+              class="block relative lg:hover:translateY-3px lg:transition-1s"
+              style="padding-top: 60%"
+              href="https://reinink.ca/eloquent-course/"
+            >
+              <img
+                class="pin absolute block rounded-t md:shadow-lg-dark"
+                src="/img/eloquent-performance-patterns.jpg?1"
+                alt="Eloquent Performance Patterns"
+              />
+            </a>
+          </div>
+          <div class="px-6 pt-8 pb-6 md:w-1/2 md:px-12 md:py-8 md:flex md:items-center">
+            <div>
+              <div
+                class="antialiased text-xl md:text-2xl font-medium"
+              >CityWallet</div>
+              <div
+                class="mt-2 md:mt-3 antialiased font-medium text-sm md:text-md leading-tight md:leading-normal opacity-75"
+              >This is a premium video course I plan to launch in 2019. My goal is to teach developers how to drastically improve the performance of their Laravel applications by pushing more work to the database, all while still using Eloquent.</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
